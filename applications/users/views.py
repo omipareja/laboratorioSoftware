5079046784d2596db6cddf0869da71144c677426
omipareja <juanmanuel.sanchez@utp.edu.co>
2021-07-23 11:03:39 -0500

    entrega final 2.0

--- applications/users/views.py
+++ applications/users/views.py
@@ -183,9 +183,9 @@ class ReclutarAdministrador(LoginRequiredMixin,FormView):
 
         try:
             #url local
-            url = self.request.META['HTTP_HOST']#si no esta en producion utilice el request
+            #url = self.request.META['HTTP_HOST']#si no esta en producion utilice el request
             #url para producion
-            #url = settings.DOMAIN
+            url = settings.DOMAIN
 
             user.token =  uuid.uuid4()
             user.save()
